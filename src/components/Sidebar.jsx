f6b26773a40f080ff0562534239c1a0e6ea3a1b8
jahan60 <faridajahan.ny@gmail.com>
2026-03-05 15:58:54 -0500

    added sidebar component and styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,29 @@
+import React from "react";
+import "../styles/sidebar.css";
+import { Link } from "react-router-dom";
 
 
-export default function Sidebar(){
-    
+ function Sidebar(){
+    return(
+        <div className="sidebar">
+            <h3 className ="sidebar_title">Menu</h3>
+
+            <ul className="sidebar_menu">
+                <li><Link to="/">Dashboard</Link></li>
+                <li><Link to="/Inventory">Inventory</Link></li>
+                <li><Link to="/alerts">Alerts</Link></li>
+                <li><Link to="/analytics">Analytics</Link></li>
+
+
+
+
+
+            </ul>
+
+
+
+        </div>
+    )
 }
+
+export default Sidebar;
